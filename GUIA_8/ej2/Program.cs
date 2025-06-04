10b28d8da01759eecca8e35a020d17b03361279d
adlfvrr <adolfover4@gmail.com>
2025-06-03 23:22:53 -0300

    commit

---
+++ GUIA_8/ej2/Program.cs
@@ -0,0 +1,102 @@
+﻿namespace ej2
+{
+    internal class Program
+    {
+        #region Declaraciones
+        static int edad0, edad1, edad2, edad3, edad,nroNina,op,suma;
+        static double monto = 0, porcentaje0 = 0, porcentaje1 = 0, porcentaje2 = 0, porcentaje3 = 0, monto0 = 0, monto1 = 0, monto2 = 0, monto3 = 0;
+        static bool seguir = true;
+        #endregion
+        #region Metodos !Mostrar
+        static void RegistrarEdad()
+        {
+            edad = 0;
+            Console.Write("Ingrese edad de la niña: ");
+            edad = Convert.ToInt32(Console.ReadLine());
+            suma += edad;
+            Console.Write("A qué niña corresponde? ");
+            nroNina = Convert.ToInt32(Console.ReadLine());
+            if (nroNina == 1)
+            {
+                edad0 = edad;
+            }
+            else if (nroNina == 2)
+            {
+                edad1 = edad;
+            }
+            else if (nroNina == 3)
+            {
+                edad2 = edad;
+            }
+            else if (nroNina == 4)
+            {
+                edad3 = edad;
+            }
+        }
+        static double RegistrarMontoARepartir()
+        {
+            Console.Write("Ingrese el monto a repartir: $");
+            monto = Convert.ToDouble(Console.ReadLine());
+            return monto;
+        }
+        static void CalcularMontosYPorcentajesARepartir()
+        {   
+                porcentaje0 = 1.0 * (edad0 / (double)suma) * 100;
+                porcentaje1 = 1.0 * (edad1 / (double)suma) * 100;
+                porcentaje2 = 1.0 * (edad2 / (double)suma) * 100;
+                porcentaje3 = 1.0 * (edad3 / (double)suma) * 100;
+                monto0 = (porcentaje0 * monto) / 100;
+                monto1 = (porcentaje1 * monto) / 100;
+                monto2 = (porcentaje2 * monto) / 100;
+                monto3 = (porcentaje3 * monto) / 100;
+        }
+        
+        #endregion
+        #region Metodos Mostrar
+        static int MostrarPantallaSolicitarOpcionMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Ingrese opción:\n1. Iniciar monto a repartir\n2. Solicitar edad por niña\n3. Mostrar monto y porcentajes que corresponde a cada niña.\nOtro. Salir");
+            op = Convert.ToInt32(Console.ReadLine());
+            return op;
+        }
+        static void MostrarPantallaSolicitarMontoARepartir()
+        {
+            RegistrarMontoARepartir();
+        }
+        static void MostrarPantallaSolicitarEdadesNiñas()
+        {
+            RegistrarEdad();
+        }
+        static void MostrarPantallaCalcularYMostrarMontoYPorcentajePorNiña()
+        {
+            if (suma > 0){
+                CalcularMontosYPorcentajesARepartir();
+                Console.WriteLine($"A la niña 1 le corresponde un {porcentaje0:f2}%, equivalente a ${monto0:f2}");
+                Console.WriteLine($"A la niña 2 le corresponde un {porcentaje1:f2}%, equivalente a ${monto1:f2}");
+                Console.WriteLine($"A la niña 3 le corresponde un {porcentaje2:f2}%, equivalente a ${monto2:f2}");
+                Console.WriteLine($"A la niña 4 le corresponde un {porcentaje3:f2}%, equivalente a ${monto3:f2}");
+                Console.ReadKey();
+            }
+            else { Console.WriteLine("No se ingresaron edades.");Console.ReadKey(); }
+        }
+        #endregion
+        #region Programa
+        static void Main(string[] args)
+        {
+            while (seguir) 
+            {
+                MostrarPantallaSolicitarOpcionMenu();
+                switch (op)
+                {
+                    case 1: MostrarPantallaSolicitarMontoARepartir(); break;
+                    case 2: MostrarPantallaSolicitarEdadesNiñas(); break;
+                    case 3: MostrarPantallaCalcularYMostrarMontoYPorcentajePorNiña(); break;
+                    default: seguir = !seguir; break;
+    
+                }
+            }
+        }
+        #endregion
+    }
+}
---
+++ GUIA_8/ej3/Program.cs
@@ -0,0 +1,162 @@
+﻿using System.Collections;
+using System.Diagnostics;
+
+namespace ej3
+{
+    internal class Program
+    {
+        #region Declaraciones
+        static string nombre0 = "", nombre1 = "", nombre2 = "", nombre,nombmayor = "",nombmedio = "",nombmenor = "";
+        static int numeroLibreta0, numeroLibreta1, numeroLibreta2, orden = 1, numero, op, notamayor = 0, notamenor = 0, notamedia = 0;
+        static bool menu = true;
+        #endregion
+        #region Metodos !Mostrar
+        static void RegistrarNombreYNumeroLibreta()
+        {
+            if (orden < 4)
+            {
+                Console.Write($"Ingrese nombre del alumno {orden}: ");
+                nombre = Console.ReadLine();
+                Console.Write("Ingrese número de libreta: ");
+                numero = Convert.ToInt32(Console.ReadLine());
+                if (nombre0 == "" && numeroLibreta0 == 0)
+                {
+                    nombre0 = nombre;
+                    numeroLibreta0 = numero;
+                }
+                else if (nombre1 == "" && numeroLibreta1 == 0)
+                {
+                    nombre1 = nombre;
+                    numeroLibreta1 = numero;
+                }
+                else if (nombre2 == "" && numeroLibreta2 == 0)
+                {
+                    nombre2 = nombre;
+                    numeroLibreta2 = numero;
+                }
+                orden++;
+                if (notamayor == 0 && notamedia == 0 && notamenor == 0)
+                {
+                    notamayor = numero;
+                    notamenor = numero;
+                    notamedia = numero;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Límite de ingresos alcanzado.");
+                Console.ReadKey();
+            }
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Hello, World!");
+            }
+        }
+        static void OrganizarLista()
+        {
+            if (numeroLibreta0 > numeroLibreta1)
+            {
+                if (numeroLibreta0 > numeroLibreta2)
+                {
+                    notamayor = numeroLibreta0;
+                    nombmayor = nombre0;
+                    if (numeroLibreta1 > numeroLibreta2)
+                    {
+                        nombmedio = nombre1;
+                        notamedia = numeroLibreta1;
+                        notamenor = numeroLibreta2;
+                        nombmenor = nombre2;
+                    }
+                    else
+                    {
+                        nombmedio = nombre2;
+                        notamedia = numeroLibreta2;
+                        notamenor = numeroLibreta1;
+                        nombmenor = nombre1;
+                    }
+                }
+                else
+                {
+                    notamayor = numeroLibreta2;
+                    nombmayor = nombre2;
+                    nombmedio = nombre0;
+                    notamedia = numeroLibreta0;
+                    notamenor = numeroLibreta1;
+                    nombmenor = nombre1;
+                }
+            }
+            else
+            {
+                if (numeroLibreta1 > numeroLibreta2)
+                {
+                    notamayor = numeroLibreta1; nombmayor = nombre1;
+                    if (numeroLibreta2 > numeroLibreta0)
+                    {
+                        notamedia = numeroLibreta2;
+                        nombmedio = nombre2;
+                        notamenor = numeroLibreta0;
+                        nombmenor = nombre0;
+                    }
+                    else
+                    {
+                        notamedia = numeroLibreta0;
+                        nombmedio = nombre0;
+                        notamenor = numeroLibreta2;
+                        nombmenor = nombre2;
+                    }
+                }
+                else
+                {
+                    notamayor = numeroLibreta2;
+                    nombmayor = nombre2;
+                    notamedia = numeroLibreta1;
+                    nombmedio = nombre1;
+                    nombmenor = nombre0;
+                    notamenor = numeroLibreta0;
+                }
+            }
+        }
+        
+        #endregion
+        #region Metodos Mostrar
+        static int MostrarPantallaSolicitarOpcionMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Ingrese opción: \n1.Registrar notas\n2.Mostrar lista ordenada\n3.Ordenar lista\nOtro.Salir");
+            op = Convert.ToInt32(Console.ReadLine());
+            return op;
+        }
+        static void MostrarPantallaSolicitarAlumnos()
+        {
+            RegistrarNombreYNumeroLibreta();
+        }
+        static void MostrarPantallaMostrarListaOrdenada()
+        {
+            if (nombmayor == "" && notamayor == 0 && nombmedio == "" && notamedia == 0 && nombmenor == "" && notamenor == 0)
+            {
+                Console.WriteLine("No se pudo determinar la lista. Es obligatorio el ingreso de 3 alumnos.");
+            }
+            else {
+                Console.WriteLine($"El alumno con mayor nota es {nombmayor} con la nota {notamayor}\nEl alumno con nota media es {nombmedio} con la nota {notamedia}\nEl alumno con menor nota es {nombmenor} con la nota {notamenor}");
+                }
+                Console.ReadKey();
+        }
+        #endregion
+        #region Programa
+        static void Main(string[] args)
+        {
+            while (menu)
+            {
+                MostrarPantallaSolicitarOpcionMenu();
+                switch (op)
+                {
+                        case 1: RegistrarNombreYNumeroLibreta();break;
+                        case 2: MostrarPantallaMostrarListaOrdenada();break;
+                        case 3: OrganizarLista();break;
+                        default: menu = !menu;break;
+                }
+            }
+        }
+        #endregion
+    }
+}
